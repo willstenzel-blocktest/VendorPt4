ba7e2c3931314e95e95fb13ee50947480d0bfbd3
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-18 10:22:00 -0500

    Added vendor object to work with external calls

--- TestVendorPt3.sol
+++ TestVendorPt3.sol
@@ -1,25 +1,27 @@
-// pragma solidity 0.4.25;
+pragma solidity 0.4.25;
 
 import "./VendorPt3.sol";
 
-contract TestVendorPt3 is VendorPt3 {
+contract TestVendorPt3 {
 
+  VendorPt3 vendor = new VendorPt3();
+  
   function test_AddItem() {
-    assert(getItemCount("Hats") == 0);
-    addItem("Hats", 15);
-    assert(getItemCount("Hats") == 15);
-    addItem("Mugs", 5);
-    addItem("Ties", 30);
-    addItem("Scarves", 20);
-    assert(getItemCount("Scarves") == 20);
+    assert(vendor.getItemCount("Hats") == 0);
+    vendor.addItem("Hats", 15);
+    assert(vendor.getItemCount("Hats") == 15);
+    vendor.addItem("Mugs", 5);
+    vendor.addItem("Ties", 30);
+    vendor.addItem("Scarves", 20);
+    assert(vendor.getItemCount("Scarves") == 20);
   }
 
   function test_PurchaseItem() {
-    addItem("Watches", 5);
-    addItem("Sunglasses", 40);
-    addItem("Water Bottles", 18);
-    assert(getItemCount("Sunglasses") == 40);
-    purchaseItem("Sunglasses");
-    assert(getItemCount("Sunglasses") == 39);
+    vendor.addItem("Watches", 5);
+    vendor.addItem("Sunglasses", 40);
+    vendor.addItem("Water Bottles", 18);
+    assert(vendor.getItemCount("Sunglasses") == 40);
+    vendor.purchaseItem("Sunglasses");
+    assert(vendor.getItemCount("Sunglasses") == 39);
   }
 }
